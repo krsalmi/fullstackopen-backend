bc8a49f9188d5c7c1dc8734377c0b05e4990699b
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-05-05 01:00:09 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -15,12 +15,27 @@ if (process.argv.length === 4) {
 
 const password = process.argv[2]
 
+console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
  const Person = mongoose.model('Person', personSchema)
