45694b1b854bc44e54b42e757efda1a014b0d9ab
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-05-05 01:04:11 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
-if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
+// if (process.argv.length < 3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
 
-if (process.argv.length === 4) {
-	console.log('to view people in phonebook, only give password. to add a new person \
-	to phonebook, give password, a name and a number')
-	process.exit(1)
-}
+// if (process.argv.length === 4) {
+// 	console.log('to view people in phonebook, only give password. to add a new person \
+// 	to phonebook, give password, a name and a number')
+// 	process.exit(1)
+// }
 
-const password = process.argv[2]
+// const password = process.argv[2]
 
 console.log('connecting to', url)
 
