5f8f53de012777c2d0903b06ee98d714c60e8838
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-05-06 01:39:23 +0300

    uibuild

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-		console.log("error")
+		console.log(error)
 		return response.status(400).send({ error: 'name must be unique' })
 	}
 
