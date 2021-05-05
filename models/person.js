3619632ac3387f6f8832919f3c4c7324d34e9e19
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-05-06 00:57:01 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
+// const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -36,6 +36,6 @@ personSchema.set('toJSON', {
 
 const Person = mongoose.model('Person', personSchema)
 
-Person.plugin(uniqueValidator);
+// Person.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Person', personSchema)
