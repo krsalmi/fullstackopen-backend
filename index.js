67eecc51620f374fa730897aacffa7e8c239db97
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-05-06 02:04:02 +0300

    uibuild

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-		if (error.errors['name'] && error.errors['name'].kind === 'unique') {
+		if (error.kind === 'unique') {
 			return response.status(400).send({ error: 'name must be unique' })
 		} else if (error.errors['name'] && error.errors['name'].kind === 'minlength') {
 				return response.status(400).send({ error: 'name must be at least 3 characters long' })
