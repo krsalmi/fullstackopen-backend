04cb6abf866a671e6d96596fc0278b8a7e2afbf5
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-05-05 17:22:59 +0300

    uibuild

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
 	const id = Number(request.params.id)
-	persons = persons.filter(p => p.id !== id)
+	persons = people.filter(p => p.id !== id)
 
 	response.status(204).end()
 })
