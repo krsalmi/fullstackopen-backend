fb42ad270e6494a048f63735591cd1a217dce7df
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-05-06 00:40:11 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,19 +1,7 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
-
-// if (process.argv.length < 3) {
-//   console.log('give password as argument')
-//   process.exit(1)
-// }
-
-// if (process.argv.length === 4) {
-// 	console.log('to view people in phonebook, only give password. to add a new person \
-// 	to phonebook, give password, a name and a number')
-// 	process.exit(1)
-// }
-
-// const password = process.argv[2]
 
 console.log('connecting to', url)
 
@@ -26,8 +14,16 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: { 
+		type: String,
+		required: true,
+		minlength: 3,
+		unique: true 
+	},
+  number: {
+		type: String,
+		minlength: 8
+	},
 })
 
 personSchema.set('toJSON', {
@@ -38,28 +34,8 @@ personSchema.set('toJSON', {
   }
 })
 
- const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema)
 
-// if (process.argv.length === 3) {
-// 	console.log('phonebook:')
-// 	Person.find({}).then(result => {
-// 		result.forEach(p => {
-// 			console.log(p.name + ' ' + p.number)
-// 		})
-// 		mongoose.connection.close()
-// 	})
-// } else {
-// 	const newName = process.argv[3]
-// 	const newNumber = process.argv[4]
-
-// 	const person = new Person({
-//   name: newName,
-//   number: newNumber,
-// })
-// 	person.save().then(response => {
-// 		console.log(`added ${newName} number ${newNumber} to phonebook`)
-// 		mongoose.connection.close()
-// 	})
-// }
+Person.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Person', personSchema)
